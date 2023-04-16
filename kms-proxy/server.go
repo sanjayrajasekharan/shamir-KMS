e8ada905e464efc4c9008afbf040febef5f3a35b
Giancarlo Pacenza <arlo.pacenza@gmail.com>
2023-04-16 15:49:47 -0400

    Add skeleton for KMS proxy server.
    The skeleton contains basic handlers for each API endpoint we expect
    to need according to our current design. There is no real logic in the
    handlers as yet, they each just return the request message with an OK
    status.

---
+++ kms-proxy/server.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type generateRootMasterKeyRequest struct {
+	KeyType     string   `json:"keyType"`
+	KeyID       string   `json:"keyID"`
+	EngineerIDs []string `json:"engineerIDs"`
+	K           int      `json:"k"`
+}
+
+type injectRootMasterKeyShareRequest struct {
+	KeyID    string `json:"keyID"`
+	KeyShare string `json:"keyShare"`
+}
+
+type encryptWithRootMasterKeyRequest struct {
+	KeyID        string `json:"keyID"`
+	PlaintextKey string `json:"plaintextKey"`
+}
+
+type decryptWithRootMasterKeyRequest struct {
+	KeyID        string `json:"keyID"`
+	EncryptedKey string `json:"encryptedKey"`
+}
+
+// Generate a root master key according to the arguments
+// in the generateRootMasterKeyRequest specified in the
+// request body. The generated key is split into n shares
+// where n is the length of `engineerIDs` in the request,
+// and can be reconstructed from `k` shares.
+func generateRootMasterKey(c *gin.Context) {
+	// Parse the request body
+	var req generateRootMasterKeyRequest
+	if err := c.BindJSON(&req); err != nil {
+		return
+	}
+	c.IndentedJSON(http.StatusOK, req)
+}
+
+// Return a share of the specified root master key
+// corresponding to the authorized caller.
+func getRootMasterKeyShare(c *gin.Context) {
+	// Parse the URL param
+	keyID := c.Param("keyId")
+	c.JSON(http.StatusOK, gin.H{"keyID": keyID})
+}
+
+// Accept a share of the specified root master key.
+func injectRootMasterKeyShare(c *gin.Context) {
+	// Parse the request body
+	var req injectRootMasterKeyShareRequest
+	if err := c.BindJSON(&req); err != nil {
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, req)
+}
+
+// Encrypt the provided plaintext key with the
+// specified root master key.
+func encryptWithRootMasterKey(c *gin.Context) {
+	// Parse the request body
+	var req encryptWithRootMasterKeyRequest
+	if err := c.BindJSON(&req); err != nil {
+		return
+	}
+	c.IndentedJSON(http.StatusOK, req)
+}
+
+// Decrypt the provided plaintext key with the
+// specified root master key.
+func decryptWithRootMasterKey(c *gin.Context) {
+	// Parse the request body
+	var req decryptWithRootMasterKeyRequest
+	if err := c.BindJSON(&req); err != nil {
+		return
+	}
+	c.IndentedJSON(http.StatusOK, req)
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/v1/keys/generate", generateRootMasterKey)
+	router.GET("/v1/keys/share/:keyId", getRootMasterKeyShare)
+	router.POST("/v1/keys/inject", injectRootMasterKeyShare)
+	router.POST("/v1/keys/encrypt", encryptWithRootMasterKey)
+	router.POST("/v1/keys/decrypt", decryptWithRootMasterKey)
+
+	router.Run("localhost:8080")
+}
