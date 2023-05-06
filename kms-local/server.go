46604f5a41c3f7bd68e34141de6edaaad0c2ff23
Giancarlo Pacenza <arlo.pacenza@gmail.com>
2023-05-06 09:52:00 -0400

    Add logic for writing new parameters to key param file when a key is generated

--- kms-local/server.go
+++ kms-local/server.go
@@ -37,7 +37,7 @@ var rootMasterKeyShares = make(map[string]map[string][]byte)
 
 type rootMasterKeyParamValues struct {
 	N                   int      `json:"n"`
-	K                   int      `json:"l"`
+	K                   int      `json:"k"`
 	KeyType             string   `json:"keyType"`
 	AuthorizedOperators []string `json:"authorizedOperators"`
 }
@@ -70,7 +70,24 @@ func contains(a string, list []string) bool {
 	return false
 }
 
-// Return the root master key share for the specifeid key corresponding to the
+func enclave_encryptWithRootMasterKeyAes256Gcm(message string, rootMasterKey []byte) (string, error) {
+	// TODO: Decrypt message using enclave private key (We assume
+	// incoming messages that require confidentiality are encrypted
+	// with the public key)
+	messageBytes := []byte(message)
+	ciphertextBytes, err := cryptoutils.EncryptAes256Gcm(rootMasterKey, messageBytes)
+	return string(ciphertextBytes), err
+}
+
+func enclave_decryptWithRootMasterKeyAes256Gcm(ciphertext string, rootMasterKey []byte) (string, error) {
+	// TODO: Do we need to encrypt a decrypt message with the enclave public key? Maybe it's easier to
+	// just assume every incoming message is encrypted
+	ciphertextBytes := []byte(ciphertext)
+	plaintextBytes, err := cryptoutils.DecryptAes256Gcm(rootMasterKey, ciphertextBytes)
+	return string(plaintextBytes), err
+}
+
+// Return the root master key share for the specified key corresponding to the
 // operator identified in `operatorCertPem`. Return an error if no root master
 // key with ID `keyID` exists, or if there is no share corresponding to the
 // operator identified in `operatorCertPem`
@@ -110,15 +127,24 @@ func enclave_GenerateAndSplitRootMasterKey(keyId string, keyType string, k int, 
 		return err
 	}
 	var sharesMap = make(map[string][]byte)
+	var authorizedOperators []string
 	for i := 0; i < n; i++ {
 		operatorCertificate := operatorCertificates[i]
 		sharesMap[operatorCertificate.Subject.CommonName] = shares[i]
+		authorizedOperators = append(authorizedOperators, operatorCertificate.Subject.CommonName)
 	}
 	rootMasterKeyShares[keyId] = sharesMap
 	rootMasterKeyParams[keyId] = rootMasterKeyParamValues{
-		K:       k,
-		N:       n,
-		KeyType: keyType,
+		K:                   k,
+		N:                   n,
+		KeyType:             keyType,
+		AuthorizedOperators: authorizedOperators,
+	}
+	file, err := json.MarshalIndent(rootMasterKeyParams, "", " ")
+	err = ioutil.WriteFile("key-params.json", file, 0644)
+	if err != nil {
+		log.Print("Error writing new prams to file.")
+		return err
 	}
 	return nil
 }
@@ -157,23 +183,6 @@ func enclave_DecryptWithRootMasterKey(message string, keyID string) (string, err
 	} else {
 		return "", errors.New(fmt.Sprintf("The specified root master key, %s, does not have a supported key type: %s", keyID, keyType))
 	}
-}
-
-func enclave_encryptWithRootMasterKeyAes256Gcm(message string, rootMasterKey []byte) (string, error) {
-	// TODO: Decrypt message using enclave private key (We assume
-	// incoming messages that require confidentiality are encrypted
-	// with the public key)
-	messageBytes := []byte(message)
-	ciphertextBytes, err := cryptoutils.EncryptAes256Gcm(rootMasterKey, messageBytes)
-	return string(ciphertextBytes), err
-}
-
-func enclave_decryptWithRootMasterKeyAes256Gcm(ciphertext string, rootMasterKey []byte) (string, error) {
-	// TODO: Do we need to encrypt a decrypt message with the enclave public key? Maybe it's easier to
-	// just assume every incoming message is encrypted
-	ciphertextBytes := []byte(ciphertext)
-	plaintextBytes, err := cryptoutils.DecryptAes256Gcm(rootMasterKey, ciphertextBytes)
-	return string(plaintextBytes), err
 }
 
 func enclave_InjectRootMasterKeyShare(keyID string, keyShare string, operatorCertificate *x509.Certificate) error {
