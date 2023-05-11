37b32af1a80e5cfd37133091f00cfacfb6b19d85
sanjay <sanjay.rajasekharan@gmail.com>
2023-05-11 07:18:31 -0400

    add host-encrypt/decrypt test files

---
+++ client/host-encrypt.py
@@ -0,0 +1,31 @@
+import argparse
+import json
+import requests
+
+# Define the headers and other parameters for the request
+headers = {"Content-Type": "application/json"}
+cert = ("operator1-cert.pem", "operator1-key.pem")
+verify = "cert.pem"
+
+# Set up command line arguments
+parser = argparse.ArgumentParser(description='Send an encrypt request to shamir KMS')
+parser.add_argument('--domain', help='The domain to send the request to.')
+parser.add_argument('--plaintext', help='The plaintext to encrypt.')
+
+args = parser.parse_args()
+
+# Define the URL for the request
+url = f"https://{args.domain}:8080/test/encrypt"
+
+# Define the payload for the request
+payload = {
+    "keyID": "abc123",
+    "plaintext": args.plaintext,  # Replace with your actual ciphertext
+}
+
+# Send the request
+response = requests.post(url, headers=headers, data=json.dumps(payload), cert=cert, verify=False)
+
+# Print the response
+print(response.status_code)
+print(response.text)
